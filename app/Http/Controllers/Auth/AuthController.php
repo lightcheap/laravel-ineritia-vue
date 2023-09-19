169e52f1365c700bb439e34c90d9cb23a1cfce2a
光安雅義 <lightcheap358@gmail.com>
2023-09-20 01:06:09 +0900

    first commit

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,50 @@
+<?php
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Str;
+
+class AuthController extends Controller
+{
+    private ?string $role;
+    private string $package;
+
+    public function __construct() {
+        $this->role = detect_role();
+        if ( empty( $this->role)) {
+            $this->package = '';
+        } else {
+            $camel = Str::camel($this->role);
+            $camel[0] = strtoupper($camel[0]);
+            $this->package =$camel;
+        }
+    }
+
+
+    public function route($route): string
+    {
+        if ( empty( $this->role)) {
+            return $route;
+        }
+        return $this->role . '.' . $route;
+    }
+
+
+    public function resource($path): string
+    {
+        if ( empty( $this->role)) {
+            return $path;
+        }
+        return $this->package . '/' . $path;
+    }
+
+    /**
+     * ロールに応じたモデルクラスの名前を取得する
+     */
+    public function modelName() {
+        if (empty($this->role)) {
+            return User::class;
+        }
+        return '\\App\\Models\\' . $this->package;
+    }
+}
---
+++ app/Http/Controllers/Auth/EmailVerificationPromptController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class EmailVerificationPromptController extends Controller
+{
+    /**
+     * Display the email verification prompt.
+     */
+    public function __invoke(Request $request): RedirectResponse|Response
+    {
+        return $this->user($request)->hasVerifiedEmail()
+                    ? redirect()->intended(RouteServiceProvider::home())
+                    : Inertia::render(
+                        $this->resource('Auth/VerifyEmail'),
+                        ['status' => session('status')]
+                    );
+    }
+}
